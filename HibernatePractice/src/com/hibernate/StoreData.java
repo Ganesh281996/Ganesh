c9529982ad727ad0660abc78a873368af3f87e7b
Ganesh <ganeshxxyyzz@gmail.com>
2018-05-22 19:22:30 +0530

    22 May 2018

---
+++ HibernatePractice/src/com/hibernate/StoreData.java
@@ -0,0 +1,30 @@
+package com.hibernate;
+
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.Transaction;  
+import org.hibernate.cfg.Configuration;  
+  
+public class StoreData 
+{ 
+	public static void main(String[] args)
+	{  
+	    Configuration cfg=new Configuration();  
+	    cfg.configure("hibernate.cfg.xml");
+	    SessionFactory factory=cfg.buildSessionFactory();  
+	    Session session=factory.openSession();  
+	    Transaction t=session.beginTransaction();  
+	          
+	    Employee e1=new Employee();  
+	    e1.setId(116);  
+	    e1.setFirstName("sonoo");  
+	    e1.setLastName("jaiswal");  
+	      
+	    session.persist(e1);
+	      
+	    t.commit();
+	    session.close();  
+	      
+	    System.out.println("successfully saved");    
+	} 
+}  
